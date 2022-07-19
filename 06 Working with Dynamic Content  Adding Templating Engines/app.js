b7b8c60a35f69daadf5855d9cc60f6e3ae486abf
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2022-07-19 20:20:36 +0200

    Templating Engines

---
+++ 06 Working with Dynamic Content  Adding Templating Engines/app.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Error 404' });
+});
+
+app.listen(3000);
